27b850d5b24e9779581826e08dd4cccd849ccf05
Lazann <48222003+Lazann@users.noreply.github.com>
2019-03-30 15:43:35 +0300

    Update hw_C1.py

--- hw_C1.py
+++ hw_C1.py
@@ -1,4 +1,5 @@
->>> a = (1,2,3,4,5,6,7,8,5,4,7,5,8,2)
->>> for i in range(1, len(a)):
+a=[int(i) for i in input("Введите числа через пробел на этой строке: ").split()]
+for i in range(1, len(a)):
 	if a[i]>a[i-1]:
 		print(a[i])
+		
